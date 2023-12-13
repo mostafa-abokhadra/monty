d46de92027f67fae6554c3e51fa5aa0b2dca1031
mostafa bukhadra <mvxizk@gmail.com>
2023-12-13 16:55:40 +0200

    ok

---
+++ 0-main.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+		int __attribute__((unused)) line_num = 0;
+
+		if (argc < 2)
+			less_than_two();
+		return (0);
+}
---
+++ 1-errors.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void less_than_two()
+{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+}
+
